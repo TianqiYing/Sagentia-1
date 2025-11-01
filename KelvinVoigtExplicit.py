6568aab2e83c95d57449c613d615a7ed5a019227
Sam Bethell <samuelbethell21@gmail.com>
2025-11-01 15:16:47 +0000

    Some changes, final model now

--- KelvinVoigtExplicit.py
+++ KelvinVoigtExplicit.py
@@ -3,42 +3,41 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 n = 10
-k =  1500*(n-2) # spring constant, n-2 since we have a dashpot
-l = 60e-3 / (n-2)  # natural length of springs
-mass = 1.8e-3 / (n-1) # mass of each small mass in chain
+k =  1500*(n-3) # spring constant, n-2 since we have a dashpot
+l = 60e-3 / (n-3)  # natural length of springs
+mass = 1.8e-3 / (n-2) # mass of each small mass in chain
 ds = 1e-3 # distance of needle to skin
 df = ds + 1.55e-3 # adding thickness of skin to ds
 dm = df + 10e-3 # adding thickness of fat to df
 di = 16e-3 # distance from beginning of muscle to point of injection
-mu_s = 5000 # stiffness of skin
+mu_s = 1000 # stiffness of skin
 mu_f = 20000 # stiffness of fat
 mu_m = 100000 # stiffness of muscle
-spring_length = 0.035 / (n-2) # compressed length of springs
-
+spring_length = 0.035 / (n-3) # compressed length of springs
 
 # plunger part
 
-Mp = 0.0012 # mass of plunger
-Ms = 0.0022 # mass of barrel
+Mp = 0.002 # mass of plunger
+Ms = 0.0035 # mass of barrel
 
 # Create mass matrix
 m = np.diag([mass]*(n-2) + [Mp] + [Ms])
 
 
-eta = 1e-3
+eta = 10 # viscocity coefficient
 Nf = 2 # normal force of seal against syringe
-mu_k = 8# coefficient of friction for seal of syringe
+mu_k = 0.5 # coefficient of friction for seal of syringe
 C_p = 0.033 # coefficicient for viscous damping
 A_p = 5.88e-5 # cross sectional area of syringe
 volume_of_fluid = 0.3e-3 # volume that needs to be injected
 mu = 0.00089 # fluid viscocity
 Ln = 16e-3 # L = needle length
-r = 0.135e-3 # needle radius
+r = 0.205e-3 # needle radius
 L = 34e-3
 
 f_max = 150000 # resistive force of skin before its punctured
 u_puncture = 2.2e-3 #  disaplacement of skin before it punctures
-s = 0.5 # coefficient of tan (how sharp is transition)
+s = 1000 # coefficient of tan (how sharp is transition)
 
 F_friction = Nf*mu_k
 
@@ -62,7 +61,7 @@ B_DD = np.zeros(n)
 B_DD[0] = -k*l
 
 dt = 0.0001
-N = 10000
+N = 100000
 
 initial_positions = np.linspace(0, spring_length, n)
 natural_positions = np.linspace(0, l, n)
@@ -74,14 +73,13 @@ a = np.zeros((n, N))
 
 # things to track
 
-for i in range(1, N): # keep track of i for future for loops
-    for i in range(1, N):
-        if u[-1, i - 1] >= ds:
-            break
-
+for i in range(1, N):
+    if u[-1, i - 1] >= ds:
+        break
+    else:
         # Update B_DD for plunger/barrel
-        B_DD[-2] = k * l - A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1])
-        B_DD[-1] = A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1])
+        B_DD[-2] = k * l + A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1])
+        B_DD[-1] = - A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1])
 
         # Compute forces if barrel sticks
         forces_if_stuck = A_DD @ u[:, i - 1] + B_DD
@@ -97,12 +95,10 @@ for i in range(1, N): # keep track of i for future for loops
         v[:, i] = v[:, i - 1] + dt * a[:, i]
         u[:, i] = u[:, i - 1] + dt * v[:, i]
 
-        # Fix first mass
+        #Fix first mass
         v[0, i] = 0
         u[0, i] = 0
 
-    else:
-        break
 
 end_phase1 = i
 
@@ -116,8 +112,8 @@ for i in range(end_phase1, N):
 
     z = s * (u[-1, i - 1] - ds - u_puncture)
     A_DD[-1, -1] = -k - mu_s * np.tanh(z)
-    B_DD[-1] = A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1]) + ds * mu_s * np.tanh(z) + f_max * (1 - np.tanh(z))
-    B_DD[-2] = k * l - A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1])
+    B_DD[-1] = - A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1]) + ds * mu_s * np.tanh(z) + f_max * (1 - np.tanh(z))
+    B_DD[-2] = k * l + A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1])
 
     forces_if_stuck = A_DD @ u[:, i - 1] + B_DD
     F_plunger_if_stuck = forces_if_stuck[-2]
@@ -140,16 +136,13 @@ end_phase2 = i
 # moving onto fat through to muscle
 
 
-
-
-
 for i in range(end_phase2, N):
     if u[-1, i - 1] >= dm:
         break
 
     A_DD[-1, -1] = -k - mu_f
-    B_DD[-1] = A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1]) - mu_s * (df - ds) + mu_f * df
-    B_DD[-2] = k * l - A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1])
+    B_DD[-1] = -A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1]) - mu_s * (df - ds) + mu_f * df
+    B_DD[-2] = k * l + A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1])
 
     forces_if_stuck = A_DD @ u[:, i - 1] + B_DD
     F_plunger_if_stuck = forces_if_stuck[-2]
@@ -177,8 +170,8 @@ for i in range(end_phase3, N):
         break
 
     A_DD[-1, -1] = -k - mu_m
-    B_DD[-1] = A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1]) - mu_s * (df - ds) - mu_f * (dm - df) + mu_m * dm
-    B_DD[-2] = k * l - A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1])
+    B_DD[-1] = -A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1]) - mu_s * (df - ds) - mu_f * (dm - df) + mu_m * dm
+    B_DD[-2] = k * l + A_p * eta / L * (v[-1, i - 1] - v[-2, i - 1])
 
     forces_if_stuck = A_DD @ u[:, i - 1] + B_DD
     F_plunger_if_stuck = forces_if_stuck[-2]
@@ -221,26 +214,22 @@ for i in range(end_phase4, N):
         + A_p * delta_p
         + C_p * v[-1, i-1]
     )
-    B_DD_internal[-2] = k * l - A_p * eta / L * (v[-1, i-1] - v[-2, i-1])
+    B_DD_internal[-2] = k * l + A_p * eta / L * (v[-1, i-1] - v[-2, i-1])
 
-    # Compute forces if barrel sticks
     forces_internal = A_DD[:-1, :-1] @ u[:-1, i-1] + B_DD_internal[:-1]
     F_plunger_if_stuck = forces_internal[-1]
 
-    # Adjust barrel force if slipping
     forces_internal_full = np.zeros(n)
     forces_internal_full[:-1] = forces_internal
     forces_internal_full[-1] = F_plunger_if_stuck
     if abs(F_plunger_if_stuck) >= F_friction:
         forces_internal_full[-1] = F_plunger_if_stuck - np.sign(F_plunger_if_stuck) * F_friction
 
-    # Solve for accelerations
     a_internal = np.linalg.solve(m, forces_internal_full)
     v[:, i] = v[:, i-1] + dt * a_internal
     u[:, i] = u[:, i-1] + dt * v[:, i]
     a[:, i] = a_internal
 
-    # Fix first mass
     v[0, i] = 0
     u[0, i] = 0
 
@@ -255,6 +244,8 @@ end_phase5 = i
 
 
 # plotting
+
+print(f"All of the fluid injected in {dt * i} seconds")
 
 u = u[:, 0:end_phase5]
 v = v[:, 0:end_phase5]
@@ -304,3 +295,6 @@ plt.plot([i for i in range(end_phase5)], Ms*a[-1, :])
 plt.xlabel("Time in seconds")
 plt.ylabel("Force of syringe")
 plt.show()
+
+
+
